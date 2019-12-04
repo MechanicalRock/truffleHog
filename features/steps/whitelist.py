4a2fd7368597fb397e0ee3967a7d7e3e60edb273
Josh Crane <josh.crane@mechanicalrock.io>
2019-12-04 19:32:36 +0800

    moved unittests back to top level

--- features/steps/whitelist.py
+++ features/steps/whitelist.py
@@ -1,13 +1,16 @@
 from behave import *
 
-@given('we have a repository with no secrets')
+
+@given("we have a repository with no secrets")
 def step_impl(context):
     pass
 
-@when('we run Trufflehog at that repository')
+
+@when("we run Trufflehog at that repository")
 def step_impl(context):
     assert True is not False
 
-@then('trufflehog should find no secrets')
+
+@then("trufflehog should find no secrets")
 def step_impl(context):
     assert context.failed is False
