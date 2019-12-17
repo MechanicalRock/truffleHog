b2db2229b05ef6fe6332a3aa99606f25f85d98ba
Josh Crane <josh.crane@mechanicalrock.io>
2019-12-17 20:00:15 +0800

    Added semver devbranch

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+from time import time
+
 
 setup(
     name="MechTruffleHog",
-    version="1.0.1",
+    version=f"1.0.0.dev{int(time())}",
     description="Searches through git repositories for high entropy strings and regex patterns. Suitable for local development and pipeline workflows.",
     url="https://github.com/MechanicalRock/truffleHog",
     author="Josh Crane",
